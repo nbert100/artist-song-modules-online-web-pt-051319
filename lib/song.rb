ce1e4732e699d5e7ec788351cf381fa6c9a17dd9
Nia Bert <nbert14@gmail.com>
2019-06-15 17:46:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
 require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
+
 class Song
   extend Memorable
   extend Findable
