7fa830519bf71df63caa005e70423d04dce0c4aa
Nia Bert <nbert14@gmail.com>
2019-06-15 17:32:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require 'pry'
 require_relative '../lib/concerns/memorable'
-# require_relative '../lib/concerns/findable.rb'
+require_relative '../lib/concerns/findable.rb'
 class Artist
   extend Memorable
   extend Findable
